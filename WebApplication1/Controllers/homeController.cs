1f8f12b57dcec93348f6a2b07bfe1b81b1980a74
Sehrishmunawer <Sehrishmunawer@LAB20-59>
2019-06-10 17:42:24 +0500

    Add project files.

---
+++ WebApplication1/Controllers/homeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class homeController : Controller
+    {
+        cmpEntities en = new cmpEntities();
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult savedata(city ct)
+        {
+            en.cities.Add(ct);
+            en.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Scripts/main.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function()
+{
+
+    $("#btn").click(function()
+    {
+       
+        var city = {
+            "CityName": $("#txtName").val()
+            
+        }
+
+        $.ajax(
+            {
+                type: "POST",
+                url: "savedata",
+                data: JSON.stringify(city),
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+              
+            });
+
+
+    });
+});
